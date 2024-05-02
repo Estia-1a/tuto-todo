86364d637fbec64c8ccaff6b118f9dede22d0366
Solène Martin <solene.martin@net.estia.fr>
2024-05-02 12:40:07 +0200

    feat(db): Création de fichiers de test de connexion (appTest.ts & database.ts) avec une bd locale MySQL

--- bd-mysql/database.ts
+++ bd-mysql/database.ts
@@ -3,21 +3,22 @@ import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 const client = await new Client().connect({
   hostname: "127.0.0.1",
   username: "root",
-  db: "db",
+  db: "db_todos",
   poolSize: 2,
-  password: "db",
+  password: "0000",
 });
-
-await client.execute(`CREATE DATABASE IF NOT EXISTS DBTodos`);
-await client.execute(`USE DBTodos`);
 
 await client.execute(`DROP TABLE IF EXISTS todos`);
 await client.execute(`
     CREATE TABLE todos (
         id int(5) NOT NULL AUTO_INCREMENT,
-        todo varchar(100) NOT NULL,
+        todo varchar(255) NOT NULL,
         PRIMARY KEY (id)
     ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 `);
+
+await client.execute(`INSERT INTO todos (todo) VALUES (?)`, ["Faire les courses"]);
+await client.execute(`INSERT INTO todos (todo) VALUES (?)`, ["Préparer le rapport"]);
+await client.execute(`INSERT INTO todos (todo) VALUES (?)`, ["Appeler le client"]);
 
 export default client;
