29280d6372d578431f0f929ad9ef7e109d86616a
Solène Martin <solene.martin@net.estia.fr>
2024-05-02 16:21:50 +0200

    feat: Connexion fonctionnelle entre l'interface React et l'API Deno à partir des requêtes

--- interface-react/src/Todo/TodoList.js
+++ interface-react/src/Todo/TodoList.js
@@ -6,30 +6,43 @@ class TodoList extends Component {
         this.state = {
             userInput: '',
             items: []
+        };
+        this.addTodo = this.addTodo.bind(this);
+    }
+
+    // Connect the enter button with the add action
+    handleKeyDown(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            this.addTodo();
         }
     }
 
-    componentDidMount() {
-        this.fetchTodos();
-    }
-
-    fetchTodos() {
-        fetch('http://localhost:8000/todos')
-            .then(response => response.json())
-            .then(data => {
-                this.setState({ items: data.data });
-            })
-            .catch(error => {
-                console.error('410:Error fetching todos:', error);
-            });
-    }
-
+    // Updates the input value with that entered by the user
     onChange(event) {
         this.setState({
             userInput: event.target.value
         });
     }
 
+    // Call the todos fetch function after the component is mounted in the DOM
+    componentDidMount() {
+        this.fetchTodos();
+    }
+
+    // Retrieve all todos by a GET request to the API
+    fetchTodos() {
+        fetch('http://localhost:8000/todos')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ items: data });
+            })
+            .catch(error => {
+                console.error('Error fetching todos:', error);
+            });
+    }
+
+    // Add a todo via a POST request to the API
     addTodo() {
         const { userInput } = this.state;
         fetch('http://localhost:8000/todos', {
@@ -39,48 +52,50 @@ class TodoList extends Component {
             },
             body: JSON.stringify({ todo: userInput }),
         })
-        .then(response => response.json())
-        .then(data => {
-            this.setState({
-                userInput: '',
-                items: [...this.state.items, data.data]
+            .then(response => response.json())
+            .then(data => {
+                this.fetchTodos();
+                this.setState({
+                    userInput: ''
+                });
+            })
+            .catch(error => {
+                console.error('Error adding todo:', error);
             });
+    }
+
+    // Delete a todo using a DELETE request to the API
+    deleteTodo(id) {
+        fetch(`http://localhost:8000/todos/${id}`, {
+            method: 'DELETE',
         })
-        .catch(error => {
-            console.error('411:Error adding todo:', error);
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Failed to delete todo');
+                }
+                return response.text();
+            })
+            .then(() => {
+                const updatedItems = this.state.items.filter(item => item.id !== id);
+                this.setState({ items: updatedItems });
+            })
+            .catch(error => {
+                console.error('Error deleting todo:', error);
+            });
+    }
+    
+    // Render the todos list
+    renderTodos() {
+        return this.state.items.map(item => {
+            return (
+                <div key={item.id}>
+                    {item.todo} | <button onClick={() => this.deleteTodo(item.id)}>X</button>
+                </div>
+            );
         });
     }
 
-    deleteTodo(index) {
-        fetch(`http://localhost:8000/todos/${index}`, {
-            method: 'DELETE',
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('412:Failed to delete todo');
-            }
-            return response.text();
-        })
-        .then(() => {
-            const updatedItems = [...this.state.items];
-            updatedItems.splice(index, 1);
-            this.setState({ items: updatedItems });
-        })
-        .catch(error => {
-            console.error('413:Error deleting todo:', error);
-        });
-    }    
-    
-    renderTodos() {
-        return this.state.items.map((item, index) => {
-            return (
-                <div key={index}>
-                    {item} | <button onClick={() => this.deleteTodo(index)}>X</button>
-                </div>
-            )
-        });
-    }    
-
+    // Render global
     render() {
         return (
             <div className='container'>
@@ -98,17 +113,18 @@ class TodoList extends Component {
                                 type='text'
                                 placeholder='Input a task...'
                                 onChange={this.onChange.bind(this)}
+                                onKeyDown={this.handleKeyDown.bind(this)}
                             />
                         </form>
                         <button className="btn btn-primary mb-2 ml-2"
-                            onClick={this.addTodo.bind(this)}>Add</button>
+                            onClick={this.addTodo}>Add</button>
                     </div>
                     <div className="row">
                         {this.renderTodos()}
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
