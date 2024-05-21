d8758c09ed5b3688e86b259b5aa751efbeeae789
Solène Martin <solene.martin@net.estia.fr>
2024-05-21 15:25:06 +0200

    feat(database): Sécurisation de la connexion à la bd via les variables d'environnement
    - Déplacement des informations de connexion vers le fichier .env
    - Mise à jour de la configuration de connexion à la bd pour utiliser les variables d'environnement

--- bd-mysql/database.ts
+++ bd-mysql/database.ts
@@ -1,12 +1,20 @@
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { join, dirname, fromFileUrl } from "https://deno.land/std/path/mod.ts";
+
+// Get current path and load environment variables from .env
+const __dirname = dirname(fromFileUrl(import.meta.url));
+const envFilePath = join(__dirname, "../.env");
+const envConfig = config({ path: envFilePath, export: true });
+
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 
-// Database connection informations
+// Database connection informations via .env
 const client = await new Client().connect({
-  hostname: "127.0.0.1",
-  username: "root",
-  db: "db_todos",
+  hostname: Deno.env.get("DB_HOSTNAME") || "127.0.0.1",
+  username: Deno.env.get("DB_USERNAME") || "root",
+  db: Deno.env.get("DB_NAME") || "db_todos",
   poolSize: 2,
-  password: "0000",
+  password: Deno.env.get("DB_PASSWORD") || "",
 });
 
 // Check if the table exists
