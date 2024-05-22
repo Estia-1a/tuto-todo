59cfe1da1d34331664686aa9d074918613f3af9e
Solène Martin <solene.martin@net.estia.fr>
2024-05-22 09:30:03 +0200

    feat(database): Ajout de la création automatique de la table users

--- bd-mysql/database.ts
+++ bd-mysql/database.ts
@@ -1,28 +1,28 @@
-import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { join, dirname, fromFileUrl } from "https://deno.land/std/path/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
+import { join, dirname, fromFileUrl } from "https://deno.land/std@0.224.0/path/mod.ts";
 
 // Get current path and load environment variables from .env
 const __dirname = dirname(fromFileUrl(import.meta.url));
 const envFilePath = join(__dirname, "../.env");
-const envConfig = config({ path: envFilePath, export: true });
+const _envConfig = config({ path: envFilePath, export: true });
 
 import { Client } from "https://deno.land/x/mysql@v2.11.0/mod.ts";
 
 // Database connection informations via .env
 const client = await new Client().connect({
-  hostname: Deno.env.get("DB_HOSTNAME") || "127.0.0.1",
-  username: Deno.env.get("DB_USERNAME") || "root",
-  db: Deno.env.get("DB_NAME") || "db_todos",
+  hostname: Deno.env.get("DB_HOSTNAME"),
+  username: Deno.env.get("DB_USERNAME"),
+  db: Deno.env.get("DB_NAME"),
   poolSize: 2,
-  password: Deno.env.get("DB_PASSWORD") || "",
+  password: Deno.env.get("DB_PASSWORD"),
 });
 
-// Check if the table exists
-const result = await client.execute(`SHOW TABLES LIKE 'todos'`);
-const tableExists = result.rows && result.rows.length > 0;
+// Check if the tables exist
+const result = await client.execute(`SHOW TABLES`);
+const tablesExist = result.rows && result.rows.length > 1;
 
-// If the table does not exist, call the installation script
-if (!tableExists) {
+// If tables don't exist, call the installation script
+if (!tablesExist) {
   const { default: setupDatabase } = await import("./install.ts");
   await setupDatabase(client);
 }
--- bd-mysql/install.ts
+++ bd-mysql/install.ts
@@ -7,6 +7,15 @@ export default async function setupDatabase(client: any) {
             PRIMARY KEY (id)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
       `);
+
+      await conn.execute(`
+        CREATE TABLE IF NOT EXISTS users (
+          id int(5) NOT NULL AUTO_INCREMENT,
+          pseudo varchar(255) UNIQUE NOT NULL,
+          mdp varchar(255) NOT NULL,
+          PRIMARY KEY (id)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+      `);
   
       await conn.execute(`INSERT INTO todos (todo) VALUES (?)`, ["Feed the ewes"]);
       await conn.execute(`INSERT INTO todos (todo) VALUES (?)`, ["Repair the wings of the mill"]);
