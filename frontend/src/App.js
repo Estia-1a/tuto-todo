20bcacb611272c6d3985c7f8d17898c1a7cd6e14
Solène Martin <solene.martin@net.estia.fr>
2024-05-23 15:55:48 +0200

    refactor(ui): Structuration du dossier frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import './App.css';
 
-import TodoList from './Todo/TodoList'; 
+import TodoList from './TodoList'; 
 
 class App extends  Component {
   render() {
